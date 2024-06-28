4b2580ca9483889f567e8207d9a1f945665aef03
huxlabs <79800703+huxlabs@users.noreply.github.com>
2024-06-28 15:38:23 -0400

    Update content

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function addShowClass(selector, delay) {
-  setTimeout(function () {
-    document.querySelector(selector).classList.add('show');
-  }, delay);
+function showContent(selectorsAndDelays) {
+  selectorsAndDelays.forEach(([selector, delay]) => {
+    setTimeout(() => {
+      document.querySelector(selector).classList.add('show');
+    }, delay);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  addShowClass('h1.animated-element', 500);
-  addShowClass('p.animated-element', 1000);
-  addShowClass('.box:first-of-type', 1500);
-  addShowClass('.box:nth-of-type(2)', 2000);
-  addShowClass('.box:nth-of-type(3)', 2500);
+  showContent([
+    ['h1.animated-element', 400],
+    ['p.animated-element', 900]
+  ]);
 });
